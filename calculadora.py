b653c8d48da6b683685413c9e61036e834bbc559
Felipe Menezes <fm08102003@gmail.com>
2026-03-09 15:28:51 -0300

    Adiciona função soma

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+# Calculadora Básica
+# Desenvolvedor A: Implementar a função de soma aqui
+# Desenvolvedor B: Implementar a função de subtração aqui
+
+# Calculadora Básica
+def soma(a, b): return a + b
+# Desenvolvedor B: Implementar a função de subtração aqui
